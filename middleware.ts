57ee708a54525fdc9e4e20807308fd63b17407a0
Mitchell Franklin <mitchellfranklin@gmail.com>
2026-08-04 19:11:06 +1000

    chore: fix middleware unused variable warning

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(_request: NextRequest) {
+export function middleware() {
   const response = NextResponse.next();
 
   response.headers.set("X-Frame-Options", "DENY");
